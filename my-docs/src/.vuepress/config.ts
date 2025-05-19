b260ea9e4d8817db28602ba3b03b3464676631de
BinaryOracle <3076679680@qq.com>
2025-05-19 22:04:10 +0800

    updates

--- my-docs/src/.vuepress/config.ts
+++ my-docs/src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/MetaMind/",
+  base: "/",
 
   lang: "zh-CN",
   title: "MetaMind",
